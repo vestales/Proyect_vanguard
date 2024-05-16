1a50c1096f3f0e13c713a9a12016dad27510b525
vestales <germanooomundo@gmail.com>
2024-05-16 15:38:06 +0200

    day_9

--- fuciones.py
+++ fuciones.py
@@ -66,3 +66,215 @@ def leer_limpiar_datos():
 
     return df_test
 
+
+def grafico_cantidad_personas(df):
+    import plotly.express as px
+
+    df_total_clientes = df.groupby(by=["process_step", "Variation", "client_id"]).count()["calls_6_mnth"].reset_index()
+    
+
+    order = ["step_1", "step_2", "step_3", "confirm"]
+
+    df_total_clientes = df_total_clientes[(df_total_clientes["process_step"].isin(order))]
+
+
+    fig = px.histogram(df_total_clientes, x="process_step", color="Variation", barmode="group")
+    fig.update_layout(xaxis=dict(categoryorder='array', categoryarray=order))
+    fig.show()
+    return None
+
+
+
+def grafico_finaliza_test():
+    import plotly.graph_objects as go
+    start_test = 26672000
+    finish_test = 18682000
+
+    tasa_fin_test = finish_test/start_test * 100
+
+    tasa_fin_test
+
+    labels = ['Finalizado','No Finalizado']
+    values = [tasa_fin_test, 100 - tasa_fin_test]
+
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
+    fig.show()
+    return None
+
+
+
+def grafico_finaliza_control():
+    import plotly.graph_objects as go
+    start_control = 23391000
+    finish_control = 15428000
+
+    tasa_fin_control = finish_control/start_control * 100
+
+    tasa_fin_control
+
+    labels = ['Finalizado','No Finalizado']
+    values = [tasa_fin_control, 100 - tasa_fin_control]
+
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
+    fig.show()
+    return None
+
+
+def grafico_tiempo_por_paso(df):
+    import plotly.express as px
+
+    df_algo = df.groupby(["step", "Variation"])["total_segons"].mean().reset_index()
+
+
+    fig = px.bar(df_algo, x="step", y="total_segons", color="Variation", barmode="group")
+    fig.show()
+    return None
+
+
+def hipotesis_test(df):
+    import scipy.stats as st
+
+    #Set the hypothesis
+
+    #H0: df_test = df_control
+    #H1: df_test > df_control
+
+    #significance level = 0.05
+
+    df_test = df[(df["Variation"].isin("Test"))]
+    df_control = df[(df["Variation"].isin("Control"))]
+    statistic, pvalue = st.ttest_ind(df_test, df_control, equal_var=False,alternative="greater")
+
+    if pvalue > 0.05:
+        print("No hay evidencia suficiente para saber si el nuevo sitio web fue mejor.")
+    else:
+        print("El nuevo sitio web dio unos resultamos mucho mejores ques el del viejo.")
+
+
+def hipotesis_tiempo(df):
+    import scipy.stats as st
+
+    #Set the hypothesis
+
+    #H0: df_test = df_control
+    #H1: df_test > df_control
+
+    #significance level = 0.05
+
+    list_rute_okey = ['start_step_1', 'step_1_step_2', 'step_2_step_3','step_3_confirm']
+
+    df_test = df[(df["step"].isin(list_rute_okey)) & (df["Variation"] == "Test")]
+
+    df_control = df[(df["step"].isin(list_rute_okey)) & (df["Variation"] == "Control")]
+    statistic, pvalue = st.ttest_ind(df_test["total_segons"], df_control["total_segons"], equal_var=False,alternative="greater")
+
+    if pvalue > 0.05:
+        print("No hay evidencia suficiente para saber si la gente del nuevo sitio web fue mas rapida que la del viejo.")
+    else:
+        print("La gente en el nuevo sitio web tardo menos tiempo ques el del viejo.")
+
+
+def hipotesis_error(df):
+    import scipy.stats as st
+
+    list_rute_reves = ['step_3_step_1',
+       'step_2_step_1', 'step_3_step_2',
+       'step_1_start', 'step_3_start', 'step_2_start']
+    
+    list_error_test = df[(df["step"].isin(list_rute_reves)) & (df["Variation"] == "Test")].groupby("client_id").count()["gendr"]
+    list_error_control = df[(df["step"].isin(list_rute_reves)) & (df["Variation"] == "Control")].groupby("client_id").count()["gendr"]
+    #Set the hypothesis
+
+    #H0: df_test = df_control
+    #H1: df_test > df_control
+
+    #significance level = 0.05
+
+    
+    statistic, pvalue = st.ttest_ind(list_error_control, list_error_test, equal_var=False,alternative="greater")
+
+    if pvalue > 0.05:
+        print("No hay evidencia suficiente para saber si la gente del nuevo sitio web tiene mas errores que la del viejo.")
+    else:
+        print("La gente en el nuevo sitio web tiene mas errores ques la del viejo.")
+
+
+def grafico_error(df):
+    import plotly.express as px
+
+    list_rute_reves = ['step_3_step_1',
+       'step_2_step_1', 'step_3_step_2',
+       'step_1_start', 'step_3_start', 'step_2_start']
+
+    df_error = df[(df["step"].isin(list_rute_reves))].groupby("step").count()["client_id"].reset_index()
+    fig = px.pie(df_error, values='client_id', names='Variation', title='Porcentaje de errores')
+    fig.show()
+
+    return None
+
+
+def grafico_error_test(df):
+    import plotly.express as px
+
+    list_rute_reves = ['step_3_step_1',
+       'step_2_step_1', 'step_3_step_2',
+       'step_1_start', 'step_3_start', 'step_2_start']
+
+    df_error = df[(df["step"].isin(list_rute_reves)) & (df["Variation"] == "Test")].groupby("step").count()["client_id"].reset_index()
+    fig = px.pie(df_error, values='client_id', names='Variation', title='Porcentaje de errores')
+    fig.show()
+
+    return None
+
+
+def grafico_error_control(df):
+    import plotly.express as px
+
+    list_rute_reves = ['step_3_step_1',
+       'step_2_step_1', 'step_3_step_2',
+       'step_1_start', 'step_3_start', 'step_2_start']
+
+    df_error = df[(df["step"].isin(list_rute_reves)) & (df["Variation"] == "Control")].groupby("step").count()["client_id"].reset_index()
+    fig = px.pie(df_error, values='client_id', names='Variation', title='Porcentaje de errores')
+    fig.show()
+
+    return None
+
+
+def hipotesis_edad(df):
+    import plotly.express as px
+    import scipy.stats as st
+
+    df_edad = df[(df["process_step"] == "confirm")].groupby(["client_id", "clnt_age", "Variation"]).count()["visit_id"].reset_index()
+    
+    df_edad_sum = df_edad.groupby(["clnt_age", "Variation"])["visit_id"].count().reset_index()
+
+    fig = px.histogram(df_edad_sum, x="clnt_age", y="visit_id" ,color="Variation", nbins=80, marginal="box")
+    fig.show()
+
+    #Set the hypothesis
+
+
+    #H0: df_edad_sum_test = df_edad_sum_control
+    #H1: df_edad_sum_test != df_edad_sum_control
+
+    #significance level = 0.05
+
+    #En este caso no podemos rechazar la hipotesis ya que son muy similares.
+    #No hay evidencia para decir que las edades son diferentes.
+
+    df_edad_sum_test = df_edad_sum[df_edad_sum["Variation"] == "Test"]
+    df_edad_sum_control = df_edad_sum[df_edad_sum["Variation"] == "Control"]
+
+    statistic, pvalue = st.ttest_ind(df_edad_sum_test['visit_id'].values, df_edad_sum_control['visit_id'].values, equal_var=False)
+
+    if pvalue > 0.05:
+        print("No hay evidencia para decir que las edades son diferentes.")
+    else:
+        print("Las edades no estan repartidas de forma uniforme.")
+
+
+
+def crear_base_de_datos(df):
+    df.to_csv("lectura_datos.csv", sep=",", index=False, encoding="utf-8")
+
--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-from fuciones import leer_limpiar_datos
-
+from fuciones import leer_limpiar_datos, grafico_cantidad_personas, grafico_tiempo_por_paso, hipotesis_test
+from fuciones import grafico_finaliza_test, grafico_finaliza_control, hipotesis_tiempo, hipotesis_error
+from fuciones import grafico_error, grafico_error_test, grafico_error_control, hipotesis_edad, crear_base_de_datos
 
 df = leer_limpiar_datos()
 
+grafico_cantidad_personas(df)
 
-df 
+grafico_finaliza_test()
+
+grafico_finaliza_control()
+
+hipotesis_test(df)
+
+grafico_tiempo_por_paso(df)
+
+hipotesis_tiempo(df)
+
+grafico_error(df)
+
+grafico_error_test(df)
+
+grafico_error_control(df)
+
+hipotesis_error(df)
+
+hipotesis_edad(df)
+
+crear_base_de_datos(df)
